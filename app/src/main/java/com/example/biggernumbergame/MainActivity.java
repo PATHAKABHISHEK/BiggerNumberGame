d469c227f78593094724b0b858a1498c0a2c52a8
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-07 20:35:06 +0530

    Added Left Button Clicked Logic

--- app/src/main/java/com/example/biggernumbergame/MainActivity.java
+++ app/src/main/java/com/example/biggernumbergame/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int num1;
     private int num2;
+    private int points = 0;
+
+    Button left_button = findViewById(R.id.left_button);
+    Button right_button = findViewById(R.id.right_button);
+
+    TextView pointsText = findViewById(R.id.points);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +37,22 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        // Acessing Buttons In Java code
-
-        Button left_button = findViewById(R.id.left_button);
-        Button right_button = findViewById(R.id.right_button);
-
         //Setting Up the text of left button and right button
 
         left_button.setText(String.valueOf(num1));
         right_button.setText(String.valueOf(num2));
 
+        pointsText.setText("Points : " + points);
     }
 
 
     public void leftButtonClicked(View view) {
+        if(num1 > num2){
+            //sucess
+            points += 10 ;
+        }else{
+            points -= 10;
+        }
     }
 
     public void rightButtonClicked(View view) {
