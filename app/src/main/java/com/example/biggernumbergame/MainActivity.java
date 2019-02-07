d43126143eb5b53990f74f1b60faaa6c488dd510
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-02-07 20:25:18 +0530

    Implemented main game logic

--- app/src/main/java/com/example/biggernumbergame/MainActivity.java
+++ app/src/main/java/com/example/biggernumbergame/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.biggernumbergame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int num1;
+    private int num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainGameLogic();
+
+
+    }
+    public void mainGameLogic(){
+        Random random = new Random();
+        while(true){
+            num1 = random.nextInt(100);
+            num2 = random.nextInt(100);
+            if(num1 != num2)
+                break;
+        }
+
+        // Acessing Buttons In Java code
+
+        Button left_button = findViewById(R.id.left_button);
+        Button right_button = findViewById(R.id.right_button);
+
+        //Setting Up the text of left button and right button
+
+        left_button.setText(String.valueOf(num1));
+        right_button.setText(String.valueOf(num2));
+
+    }
+
+
+    public void leftButtonClicked(View view) {
+    }
+
+    public void rightButtonClicked(View view) {
     }
 }
